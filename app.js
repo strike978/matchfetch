3dedfe31e7854d6042b0e7e47d363cf0288b2cae
strike978 <onunez978@gmail.com>
2026-07-25 20:45:05 -0400

    feat: Add label for match count input in fetch options

--- app.js
+++ app.js
@@ -410,7 +410,7 @@
             html += '<option value="' + guid + '">' + name + '</option>';
         }
         html += '</select><button id="clearKitBtn" class="clear-btn" title="Clear kit data" hidden><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg></button></div>';
-        html += '<div class="fetch-row"><input type="number" id="matchCountInput" class="count-input" value="100" min="1" step="1"><button class="btn fetch-list-btn" id="fetchListBtn" disabled><span>&#x25B6;</span> Fetch</button></div>';
+        html += '<div class="label" style="margin-top:16px">Fetch Options</div><div class="fetch-row"><label class="input-label">Matches <input type="number" id="matchCountInput" class="count-input" value="100" min="1" step="1"></label><button class="btn fetch-list-btn" id="fetchListBtn" disabled><span>&#x25B6;</span> Fetch</button></div>';
         html += '<div id="statusMsg" class="status-msg"></div>';
         html += '<div id="matchListResult"></div>';
 
